32d025f7f2999d7bb25d44a6bf34d43220447260
Joshjay05 <Joshjay05>
2023-04-04 15:32:47 +0100

    updated

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import logo from "./img/logo.jpeg";
 import "./header.css";
-import Navbar from "./Navbar";
+// import Navbar from "./Navbar";
 
 const Header = () => {
   return (
